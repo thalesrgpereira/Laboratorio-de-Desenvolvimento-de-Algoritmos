e8f77dc75a0f594cd1d48c5a4784d9da84d90533
thalesrgpereira <47888546+thalesrgpereira@users.noreply.github.com>
2019-10-02 13:31:11 -0300

    Add files via upload

---
+++ Pratica de Fixacao para 1a Avaliacao/exercicio1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int i, vetor[5];
+	printf("Informe 5 numeros:");
+	for(i = 0; i < 5; i++) 
+		scanf(" %d",&vetor[i]);
+	printf("Vetor em ordem crescente: ");
+	for(i = 0; i < 5 ; i++ ) 
+	    printf("%d ",vetor[i]);
+	printf("\nVetor em ordem decrescente: ");
+	for(i = 4; i >= 0 ; i-- ) 
+	    printf("%d ",vetor[i]);
+	return 0;
+}
