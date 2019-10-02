29e6495a78b90f5d4fc70846b75be5c9f5c2eba7
thalesrgpereira <47888546+thalesrgpereira@users.noreply.github.com>
2019-10-02 13:42:02 -0300

    Add files via upload

---
+++ Pratica de Fixacao para 1a Avaliacao/exercicio4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	char i, c, vetor[50];
+	printf("Digite um nome:");
+	for(i = 0; i < 50 && c != '\n'; i++) {
+		c = getc(stdin);
+		if(c != '\n') vetor[i] = c;
+	}
+	printf("Seu nome impresso de tres formas:\n");
+	printf("%s\n",vetor);
+	for(i = 0; i < strlen(vetor); i++) putchar(vetor[i]);
+	putchar('\n');
+	for(i = strlen(vetor) -1; i >= 0 ; i--) putchar(vetor[i]);
+	return 0;
+}
